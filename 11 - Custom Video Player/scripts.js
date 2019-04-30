ddcb2f55348534e19f1cf5ca94a7fab1f993c83a
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-04-30 17:16:30 +0100

    Update progress bar as video plays

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,10 +6,16 @@ function $all (selector) {
 }
 
 const video = $("video.player__video");
+const progressFilled = $('.progress__filled');
 const playPauseButton = $('button[title="Toggle Play"]');
 
 const playIcon = '►';
 const pauseIcon = '❚ ❚';
+
+video.ontimeupdate = function (event) {
+  const percentageComplete = (video.currentTime / video.duration) * 100;
+  progressFilled.style['flex-basis'] = `${percentageComplete}%`;
+}
 
 playPauseButton.onclick = function () {
   video.paused ? video.play() : video.pause();
