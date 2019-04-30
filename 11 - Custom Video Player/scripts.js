9565b0796eeb6e43f829d70097ed8b466eea3820
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-04-30 17:47:34 +0100

    Make skip back button go back 10 seconds

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -8,6 +8,7 @@ function $all (selector) {
 const video = $("video.player__video");
 const progressFilled = $('.progress__filled');
 const playPauseButton = $('button[title="Toggle Play"]');
+const skipBackButton = $('button[data-skip="-10"]');
 
 const playIcon = '►';
 const pauseIcon = '❚ ❚';
@@ -15,9 +16,13 @@ const pauseIcon = '❚ ❚';
 video.ontimeupdate = function (event) {
   const percentageComplete = (video.currentTime / video.duration) * 100;
   progressFilled.style['flex-basis'] = `${percentageComplete}%`;
-}
+};
 
 playPauseButton.onclick = function () {
   video.paused ? video.play() : video.pause();
   playPauseButton.textContent = video.paused ? playIcon : pauseIcon;
+};
+
+skipBackButton.onclick = function () {
+  video.currentTime = video.currentTime - 10;
 };
