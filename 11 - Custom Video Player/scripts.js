19c42f5ebfbd4e573daa3b43ed6955be5cb81867
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2019-05-03 14:51:30 +0100

    Add seeking
    This could be improved with consideration of what happens when you move
    in and out of the progress bar with mouse button held down.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ function $all (selector) {
 }
 
 const video = $("video.player__video");
+const progress = $('.progress');
 const progressFilled = $('.progress__filled');
 const playPauseButton = $('button[title="Toggle Play"]');
 const skipBackButton = $('button[data-skip="-10"]');
@@ -16,10 +17,26 @@ const playbackRateInput = $('input[name="playbackRate"]');
 const playIcon = '►';
 const pauseIcon = '❚ ❚';
 
+let isSeeking = false;
+
 video.ontimeupdate = function () {
   const percentageComplete = (video.currentTime / video.duration) * 100;
   progressFilled.style['flex-basis'] = `${percentageComplete}%`;
 };
+
+progress.addEventListener('mousedown', function () {
+  isSeeking = true;
+});
+
+progress.addEventListener('mousemove', function ({ offsetX }) {
+  if (isSeeking) {
+    video.currentTime = (offsetX / progress.clientWidth) * video.duration;
+  }
+});
+
+progress.addEventListener('mouseup', function () {
+  isSeeking = false;
+});
 
 playPauseButton.onclick = function () {
   video.paused ? video.play() : video.pause();
